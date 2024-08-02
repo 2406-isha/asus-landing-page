cf594af3e2dcf03d5c5142f54990828834192f6a
2406-isha <koshtiisha510@gmail.com>
2024-08-02 13:50:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your inquiry! We will get back to you soon.');
+    // Here you can add code to send the form data to your backend or an email service
+});
